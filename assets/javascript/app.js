146ca9b8ad2ea2dea11ec62d3cd0ffe7505ffbc3
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-12-11 19:10:53 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDkWIDbHJgspkk_IHcw0MqkYHGbe8Xy7HQ",
+    authDomain: "group-project-1-318b8.firebaseapp.com",
+    databaseURL: "https://group-project-1-318b8.firebaseio.com",
+    projectId: "group-project-1-318b8",
+    storageBucket: "group-project-1-318b8.appspot.com",
+    messagingSenderId: "426857147168"
+  };
+  
+  firebase.initializeApp(config);
