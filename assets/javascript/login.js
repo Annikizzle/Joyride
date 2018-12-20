7048e1e31d3a4c9c8c9b2cbcb4309ad18cd737d1
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2018-12-20 13:13:19 -0500

    Add files via upload

---
+++ assets/javascript/login.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyDkWIDbHJgspkk_IHcw0MqkYHGbe8Xy7HQ",
+        authDomain: "group-project-1-318b8.firebaseapp.com",
+        databaseURL: "https://group-project-1-318b8.firebaseio.com",
+        projectId: "group-project-1-318b8",
+        storageBucket: "group-project-1-318b8.appspot.com",
+        messagingSenderId: "426857147168"
+      };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+
+$(".register form").on("submit", function(event){
+    event.preventDefault();
+
+    var email = $(".register .email").val();
+    var password = $(".register .password").val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(user){
+            console.log(user);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+
+
+
+    });
+
+$(".register form").on("submit", function(event){
+    event.preventDefault();
+
+    var email = $(".register .email").val();
+    var password = $(".register .password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(user){
+            console.log(user);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+
+    });
+
+    firebase.auth().onAuthStateChanged(function(user){
+        if(user){
+            console.log("User is signed in.");
+        } else {
+            console.log("No user logged in..")
+        }
+    });
+
+});
